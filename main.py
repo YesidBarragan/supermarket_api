ef129ef85ce366cac81ee983f917716b2b812f41
YesidBarragan <yesarmand2006@hotmail.com>
2020-12-11 14:26:49 -0500

    Delete client

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
 
+#listar clientes
+@api.get("/clients/")
+async def get_all_clients():
+    return database_clients
+
 #Consultar Cliente
 @api.get("/client/search/")
 async def search_client(client_search: ClientSearch):
@@ -29,3 +34,10 @@ async def search_client(client_in_db: ClientInDB):
 async def update_client(client_in_db: ClientInDB):
     database_clients[client_in_db.cedula]=client_in_db
     return database_clients[client_in_db.cedula]
+
+#Eliminar Cliente
+@api.post("/client/delete/")
+async def update_client(client_in_db: ClientInDB):
+    if database_clients[client_in_db.cedula]:
+        database_clients.pop(client_in_db.cedula)
+        raise HTTPException(status_code=500, detail="El cliente fue borrado con exito")
